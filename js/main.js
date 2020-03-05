c841ad55c8e9804f2045cd3c382a4a9000341c67
Francesco Pagano <fra.pgn@protonmail.com>
2020-03-05 16:15:53 +0100

    Invio tramite tasto Enter

--- js/main.js
+++ js/main.js
@@ -5,8 +5,35 @@ var minutes = data.getMinutes();
 var currentTime = hours + ':' + minutes;
 console.log(currentTime);
 
+// Azione invio tramite icona
 $('.sent-msg').click(function() {
-    // Invio messaggio
+    sentMessage();
+    randomReply();
+});
+
+// Azione invio tramite tasto Enter
+$('#write-msg').keydown(function(event) {
+    switch (event.key) {
+        case 'Enter':
+            sentMessage();
+            randomReply();
+            break;
+        default:
+
+    }
+});
+
+// Focus sull'input Scrivi un messaggio --> Scompare icona microfono e compare icona invio
+$('#write-msg').focus(function() {
+    $('.record-audio').hide();
+    $('.sent-msg').show();
+});
+
+// Filtro ricerca amici
+
+// FUNZIONI /////////////////////////////////////////////////////
+// Invio messaggio
+function sentMessage() {
     var writeMsg = $('#write-msg').val();
     console.log(writeMsg);
     $('#write-msg').val('');
@@ -15,8 +42,10 @@ $('.sent-msg').click(function() {
     $(clonedSent).find('.sent-text').text(writeMsg);
     $(clonedSent).find('.sent-time').text(currentTime);
     $('.history-messages-container').append(clonedSent);
+}
 
-    // Risposta random
+// Messaggio di risposta random
+function randomReply() {
     setTimeout(function() {
         var clonedReceived = $('.templates-msgs .row-received').clone();
         console.log(clonedReceived);
@@ -24,10 +53,4 @@ $('.sent-msg').click(function() {
         $(clonedReceived).find('.received-time').text(currentTime);
         $('.history-messages-container').append(clonedReceived);
     }, 1000);
-
-})
-
-$('#write-msg').focus(function() {
-    $('.record-audio').hide();
-    $('.sent-msg').show();
-})
+}
